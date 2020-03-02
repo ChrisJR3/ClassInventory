1d3aba1864712decb81a765fddc7b693cc7b1f15
Christopher Rouse <chrirous315@ed.amdsb.ca>
2020-03-02 12:37:26 -0500

    Version #2

--- ClassInventory/Form1.cs
+++ ClassInventory/Form1.cs
@@ -44,9 +44,20 @@ namespace ClassInventory
             // Lambda Expressions.
             //---------------------------
 
-            // TODO - if object is in list remove it
-
-            // TODO - display message to indicate deletion made
+            // if object is in list remove it
+            // display message to indicate deletion made
+           // Player_Info search = allPlayers.Find(x => x.name == textBox1.Text);
+            int index = allPlayers.FindIndex(x => x.name == removeInput.Text);
+            if (index < 0)
+            {
+                outputLabel.Text = "No player matches that name.";
+            }
+            else
+            {
+                //int converter = Convert.ToInt32(search);
+                allPlayers.RemoveAt(index);
+                outputLabel.Text = "Player removed sucessfuly.";
+            }
         }
 
         private void searchButton_Click(object sender, EventArgs e)
@@ -54,18 +65,29 @@ namespace ClassInventory
             // This is to be completed in Part II. You will use 
             // Lambda Expressions.
             //---------------------------
+            outputLabel.Text = "";
 
-            // TODO - if object entered exists in list show it
-            // TODO - else show not found message
+            // if object entered exists in list show it
+            // else show not found message
+            Player_Info search = allPlayers.Find(x => x.name == textBox1.Text);
+            if (search == null)
+            {
+                outputLabel.Text = "No player matches that name";
+            }
+            else
+            {
+                outputLabel.Text = "Name:    " + search.name + "\n" + "Age:       " + search.age + "\n" + "Team:     " + search.team + "\n" + "Position: " + search.position;
+            }
         }
 
         private void showButton_Click(object sender, EventArgs e)
         {
-            // This is to be completed in Part II. You will use 
-            // Lambda Expressions.
-            //---------------------------
-
-            // TODO - show all objects in list
+            outputLabel.Text = "";
+            foreach (Player_Info p in allPlayers)
+            {
+                outputLabel.Text += "\n" + "\n" + "Name:    " + p.name + "\n" + "Age:       " + p.age + "\n" + "Team:     " + p.team + "\n" + "Position: " + p.position;
+            }
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
